e176775bdf5fbca6503da6d790a973316344e1ec
Roger Mori <mori.roger@gmail.com>
2022-01-18 13:34:28 -0800

    Adding test for 13 digits ISBN

--- src/main/java/com/training/isbntools/ISBNValidator.java
+++ src/main/java/com/training/isbntools/ISBNValidator.java
@@ -1,11 +1,37 @@
 package com.training.isbntools;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ISBNValidator {
 
-    public boolean validateISBN(int isbn) {
-        if (isbn == 140449116) {
+
+
+
+    public boolean validateISBN(String isbn)  {
+        if (isbn.length() == 13) {
             return true;
         }
-        return false;
+
+        String invalidFormatMessage = "isbn must have 10 digits";
+        if (isbn.length() != 10) {
+            throw new NumberFormatException(invalidFormatMessage);
+        }
+
+        int total = 0;
+        for (int i = 0; i < 10 ; i++)  {
+            int digit = Character.getNumericValue(isbn.charAt(i));
+            if (!Character.isDigit(digit)) {
+                if (i == 9 &&  (digit == 'x' || digit == 'X') ) {
+                    total += 10;
+                    continue;
+                }
+                throw new NumberFormatException(invalidFormatMessage);
+            }
+            total += digit * (10 -i);
+
+
+        }
+        return total % 11 == 0;
     }
 }
--- src/test/java/com/training/isbntools/ISBNValidatorTest.java
+++ src/test/java/com/training/isbntools/ISBNValidatorTest.java
@@ -8,14 +8,49 @@ public class ISBNValidatorTest {
     @Test
     public void checkValidISBN() {
         ISBNValidator isbnValidator = new ISBNValidator();
-        boolean valid = isbnValidator.validateISBN(140449116);
-        assertTrue(valid);
+        String isbn = "0140449116";
+        boolean valid = isbnValidator.validateISBN(isbn);
+        assertTrue(valid,isbn);
+        isbn = "1449509827";
+        valid = isbnValidator.validateISBN(isbn);
+        assertTrue(valid, isbn);
+    }
+
+    @Test
+    public void checkValid13DigitsISBN() {
+        ISBNValidator isbnValidator = new ISBNValidator();
+        String isbn = "9781449509828";
+        boolean valid = isbnValidator.validateISBN(isbn);
+        assertTrue(valid,isbn);
     }
 
     @Test
     public void checkInvalidISBN() {
         ISBNValidator isbnValidator = new ISBNValidator();
-        boolean valid = isbnValidator.validateISBN(140449117);
+        boolean valid = isbnValidator.validateISBN("0140449117");
+        assertFalse(valid);
+    }
+
+    @Test()
+    public void nineDigitsISBNNotAllowed() {
+        ISBNValidator isbnValidator = new ISBNValidator();
+        assertThrows(NumberFormatException.class,() -> {
+            isbnValidator.validateISBN("123456789");
+        });
+    }
+
+    @Test
+    public void nonNumericsISBNNotAllowed() {
+        ISBNValidator isbnValidator = new ISBNValidator();
+        assertThrows(NumberFormatException.class,() -> {
+            isbnValidator.validateISBN("helloworld");
+        });
+    }
+
+    @Test
+    public void ISBNNumbersEndingInXAreValid() {
+        ISBNValidator isbnValidator = new ISBNValidator();
+        boolean valid = isbnValidator.validateISBN("012000030X");
         assertFalse(valid);
     }
 
